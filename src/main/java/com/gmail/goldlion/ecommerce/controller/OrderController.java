97fb8471f05607c7cd99b6083bf6c15f90bdf5c0
javsper <goldlion1219@gmail.com>
2023-03-04 12:11:07 +0300

    Fixed controller, domain, mapper, service classes.

--- src/main/java/com/gmail/goldlion/ecommerce/controller/OrderController.java
+++ src/main/java/com/gmail/goldlion/ecommerce/controller/OrderController.java
@@ -1,54 +1,35 @@
 package com.gmail.goldlion.ecommerce.controller;
 
-import com.gmail.goldlion.ecommerce.domain.User;
 import com.gmail.goldlion.ecommerce.dto.OrderDto;
-import com.gmail.goldlion.ecommerce.dto.PerfumeDto;
 import com.gmail.goldlion.ecommerce.exception.InputFieldException;
 import com.gmail.goldlion.ecommerce.mapper.OrderMapper;
-import com.gmail.goldlion.ecommerce.mapper.UserMapper;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/order")
 public class OrderController {
 
-    private final UserMapper userMapper;
     private final OrderMapper orderMapper;
 
-    public OrderController(UserMapper userMapper, OrderMapper orderMapper) {
-        this.userMapper = userMapper;
+    public OrderController(OrderMapper orderMapper) {
         this.orderMapper = orderMapper;
     }
 
-    @GetMapping
-    public ResponseEntity<List<PerfumeDto>> getOrder(@AuthenticationPrincipal User user) {
-        return ResponseEntity.ok(userMapper.getCart(user.getEmail()));
-    }
-
     @PostMapping
-    public ResponseEntity<OrderDto> postOrder(@AuthenticationPrincipal User user,
-                                              @Valid @RequestBody OrderDto orderDto,
-                                              BindingResult bindingResult) {
+    public ResponseEntity<OrderDto> postOrder(@Valid @RequestBody OrderDto orderDto, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             throw new InputFieldException(bindingResult);
         } else {
-            return ResponseEntity.ok(orderMapper.postOrder(orderDto, user.getEmail()));
+            return ResponseEntity.ok(orderMapper.postOrder(orderDto));
         }
     }
 
     @GetMapping("/finalize")
     public ResponseEntity<Long> finalizeOrder() {
         return ResponseEntity.ok(orderMapper.finalizeOrder());
-    }
-
-    @GetMapping("/list")
-    public ResponseEntity<List<OrderDto>> getUserOrdersList(@AuthenticationPrincipal User user) {
-        return ResponseEntity.ok(orderMapper.findOrderByUser(user));
     }
 }
