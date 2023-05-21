57e41e77bb442fc3ad627ecc107adaed9c1d004a
javsper <goldlion1219@gmail.com>
2023-05-21 21:42:32 +0300

    Added order and user GraphQL queries.

--- src/main/java/com/gmail/goldlion/ecommerce/mapper/PerfumeMapper.java
+++ src/main/java/com/gmail/goldlion/ecommerce/mapper/PerfumeMapper.java
@@ -4,7 +4,7 @@ import com.gmail.goldlion.ecommerce.domain.Perfume;
 import com.gmail.goldlion.ecommerce.dto.perfume.PerfumeRequestDto;
 import com.gmail.goldlion.ecommerce.dto.perfume.PerfumeResponseDto;
 import com.gmail.goldlion.ecommerce.service.PerfumeService;
-import com.gmail.goldlion.ecommerce.service.graphql.GraphQLPerfumeProvider;
+import com.gmail.goldlion.ecommerce.service.graphql.GraphQLProvider;
 import graphql.ExecutionResult;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
@@ -20,7 +20,7 @@ public class PerfumeMapper {
 
     private final ModelMapper modelMapper;
     private final PerfumeService perfumeService;
-    private final GraphQLPerfumeProvider perfumeProvider;
+    private final GraphQLProvider perfumeProvider;
 
     private Perfume convertToEntity(PerfumeRequestDto perfumeRequestDto) {
         return modelMapper.map(perfumeRequestDto, Perfume.class);
